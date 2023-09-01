50091861f922462c3325b4c05071ade9d6f0d0f8
Faizan Syed <faizan.syed1203@gmail.com>
2023-08-31 21:05:43 -0400

    Added a check to ensure that the demolink exists to show the button

--- app/src/components/Projects/ProjectCard/ProjectCard.tsx
+++ app/src/components/Projects/ProjectCard/ProjectCard.tsx
@@ -14,7 +14,13 @@ function ProjectCard(project: ProjectProps) {
   return (
     <Card className="project-card-view">
       <Card.Img variant="top" src={project.imgPath} alt="card-img" />
-      <Card.Body style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+      <Card.Body
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+        }}
+      >
         <Card.Title>{project.title}</Card.Title>
         <Card.Text style={{ textAlign: 'justify' }}>
           {project.description}
@@ -31,17 +37,21 @@ function ProjectCard(project: ProjectProps) {
               Github
             </Button>
           </Col>
-          <Col md={6}>
-            <Button
-              className="viewbtn"
-              variant="outline-secondary"
-              href={project.demoLink}
-              target="_blank"
-              rel="noreferrer"
-            >
-              Demo
-            </Button>
-          </Col>
+          {project.demoLink ? (
+            <Col md={6}>
+              <Button
+                className="viewbtn"
+                variant="outline-secondary"
+                href={project.demoLink}
+                target="_blank"
+                rel="noreferrer"
+              >
+                Demo
+              </Button>
+            </Col>
+          ) : (
+            ''
+          )}
         </Row>
       </Card.Body>
     </Card>
